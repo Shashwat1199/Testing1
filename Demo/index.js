ec0ec690d007ccbfe49ade903f2691aed4fa5106
Shashwat1199 <shashwatsanskar@gmail.com>
2023-09-09 21:20:54 +0530

    fourth commit

--- Demo/index.js
+++ Demo/index.js
@@ -1,7 +1,13 @@
 
+// var ulTag = document.getElementsByTagName('li');
+// ulTag[2].style.backgroundColor = 'green';
+// for(let i = 0;i<ulTag.length;i++)
+// ulTag[i].style.fontWeight = 'bold';
+
 var ulTag = document.getElementsByClassName('list-group-item');
 ulTag[2].style.backgroundColor = 'green';
 for(let i = 0;i<ulTag.length;i++)
 ulTag[i].style.fontWeight = 'bold';
+
 
 
