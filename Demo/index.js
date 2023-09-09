aa222697850e467c425aa46d0a3a043c96c07979
Shashwat1199 <shashwatsanskar@gmail.com>
2023-09-10 00:47:00 +0530

    sixth commit

--- Demo/index.js
+++ Demo/index.js
@@ -1,4 +1,27 @@
- 
-var ulTag = document.querySelectorAll('li');
-ulTag[1].style.backgroundColor = 'green'
-ulTag[2].style.color = 'white'
+var addItem = document.querySelector('#main-header')
+addItem.children[0].style.color = 'red';
+console.log(addItem.children)
+
+var newDiv = document.createElement('id')
+newDiv.id = 'hello'
+
+var newDivText = document.createTextNode('Hello World!')
+
+ newDiv.appendChild(newDivText)
+
+ var container = document.querySelector('header .container')
+ var h1 =   document.querySelector('header h1')
+
+ container.insertBefore(newDiv,h1);     
+
+var newDiv = document.createElement('id')
+newDiv.id = 'hello'
+
+var newDivText = document.createTextNode('Hello World!')
+
+ newDiv.appendChild(newDivText)
+
+ var container = document.querySelector('#main') 
+ var h1 = document.querySelector('#items')
+
+ container.insertBefore(newDiv,h1);   
