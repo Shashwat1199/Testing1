175c95af560fd8390dfac078ae543d094f8099a7
Shashwat1199 <shashwatsanskar@gmail.com>
2023-09-09 22:37:25 +0530

    fifth commit

--- Demo/index.js
+++ Demo/index.js
@@ -1,13 +1,4 @@
-
-// var ulTag = document.getElementsByTagName('li');
-// ulTag[2].style.backgroundColor = 'green';
-// for(let i = 0;i<ulTag.length;i++)
-// ulTag[i].style.fontWeight = 'bold';
-
-var ulTag = document.getElementsByClassName('list-group-item');
-ulTag[2].style.backgroundColor = 'green';
-for(let i = 0;i<ulTag.length;i++)
-ulTag[i].style.fontWeight = 'bold';
-
-
-
+ 
+var ulTag = document.querySelectorAll('li');
+ulTag[1].style.backgroundColor = 'green'
+ulTag[2].style.color = 'white'
