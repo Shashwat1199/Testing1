003995a9aed495305e00afb6643d20d4b734d51f
Shashwat1199 <shashwatsanskar@gmail.com>
2023-09-09 20:53:21 +0530

    third commit

--- Demo/index.js
+++ Demo/index.js
@@ -1,22 +1,7 @@
-console.log(document.domain);
-console.log('hello');
 
-console.log(document.doctype);
-console.log(document.body);
-console.log(document.body.children[2]);
-console.log(document.body.children[1]);
-console.log(document.all);
-var ul = (document.getElementById('items'));
-console.log(ul.getAttribute);
-var headerTitle = document.getElementById('header-title');
-//headerTitle.textContent = 'hello';
-headerTitle.style.borderBottom = 'solid 3px black';
-console.log(headerTitle);
-var header = document.getElementById('main-header');
-header.style.borderBottom = 'solid 3px black';
-var addItem = document.getElementsByClassName('title');
-addItem[0].style.fontWeight = 'bold';
-addItem[0].style.color = 'green'
-
+var ulTag = document.getElementsByClassName('list-group-item');
+ulTag[2].style.backgroundColor = 'green';
+for(let i = 0;i<ulTag.length;i++)
+ulTag[i].style.fontWeight = 'bold';
 
 
