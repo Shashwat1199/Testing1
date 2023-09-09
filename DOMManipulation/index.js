03689b8d2adf596c1ee04436d2890c4b4228cc76
Shashwat1199 <shashwatsanskar@gmail.com>
2023-09-09 17:32:22 +0530

    first commit

---
+++ DOMManipulation/index.js
@@ -0,0 +1,6 @@
+// const ul = document.body.children[0];
+// const ulTag = ul.children[3];
+// console.log(ulTag);
+const ul = document.getElementsByClassName('item');
+const ulTag = ul.namedItem()
+console.log(ulTag);
