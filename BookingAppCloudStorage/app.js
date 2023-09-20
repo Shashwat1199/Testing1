aabc6145aceb9ee71561c81a89eae5057fdd5b1d
Shashwat1199 <shashwatsanskar@gmail.com>
2023-09-20 15:52:30 +0530

    deleting the info on cloud

--- BookingAppCloudStorage/app.js
+++ BookingAppCloudStorage/app.js
@@ -8,14 +8,17 @@ let li;
 myForm.addEventListener('submit', onSubmit);
 //Delete an element
 userList.addEventListener('click', removeItem);
+//Delete from cloud storage
+//userList.addEventListener('click', removeFromCloud);
 //edit an item
 userList.addEventListener('click', editItem);
 
 window.addEventListener('DOMContentLoaded',()=>{
-    axios.get('https://crudcrud.com/api/ce86e8b862894f29b21c2ac198952d75/BooKAppointment')
+    axios.get('https://crudcrud.com/api/9f269f43c52442c8887cd5c6912fd766/BooKAppointment')
     .then((response)=>{
         for(var i = 0;i<response.data.length;i++)
         {
+            //console.log(response.data[i])
             showSavedUserOnScreen(response.data[i]);
         }
     })
@@ -26,6 +29,7 @@ window.addEventListener('DOMContentLoaded',()=>{
 
 function showSavedUserOnScreen(user)
 {
+
     if(user.name === '' || user.email === '') {
         // alert('Please enter all fields');
         msg.classList.add('error');
@@ -38,27 +42,13 @@ function showSavedUserOnScreen(user)
         li = document.createElement('li');
     
         // Add text node with input values
-        li.appendChild(document.createTextNode(`${user.name}  ${user.email}` ));
+        li.appendChild(document.createTextNode(`${user.name}-${user.email} ` ));
     
         // Append to ul
         userList.appendChild(li);     
-      
-        //Adding more than one users
-         myObj = {
-          name : user.name,
-          email : user.email
-        };
-            
-        axios.post('https://crudcrud.com/api/ce86e8b862894f29b21c2ac198952d75/BookAppointment',myObj)
-        .then((response)=>{
-            console.log(response);
-        })
-        .catch((err)=>{
-            console.log(err);
-        })  
-         
-            // Create del button element
-          var deleteBtn = document.createElement('button');
+                 
+        // Create del button element
+        var deleteBtn = document.createElement('button');
     
          // Add classes to del button
          deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
@@ -100,7 +90,7 @@ function onSubmit(e) {
     li = document.createElement('li');
 
     // Add text node with input values
-    li.appendChild(document.createTextNode(`${nameInput.value}  ${emailInput.value}` ));
+    li.appendChild(document.createTextNode(`${nameInput.value}-${emailInput.value} ` ));
 
     // Append to ul
     userList.appendChild(li);     
@@ -111,61 +101,96 @@ function onSubmit(e) {
       email : emailInput.value
     };
         
-    axios.post('https://crudcrud.com/api/ce86e8b862894f29b21c2ac198952d75/BookAppointment',myObj)
+    axios.post('https://crudcrud.com/api/9f269f43c52442c8887cd5c6912fd766/BookAppointment',myObj)
     .then((response)=>{
-        console.log(response);
+        //console.log(response);
     })
     .catch((err)=>{
         console.log(err);
     })  
      
-        // Create del button element
-      var deleteBtn = document.createElement('button');
+    // Create del button element
+    var deleteBtn = document.createElement('button');
 
-     // Add classes to del button
-     deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+    // Add classes to del button
+    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
 
-  // Append text node
-     deleteBtn.appendChild(document.createTextNode('X'));
+    // Append text node
+    deleteBtn.appendChild(document.createTextNode('X'));
 
-  // Append button to li
-     li.appendChild(deleteBtn);
+    // Append button to li
+    li.appendChild(deleteBtn);
 
     //Edit Button 
-     var editBtn = document.createElement('button');
+    var editBtn = document.createElement('button');
 
-     // Add classes to edit button
+    // Add classes to edit button
      editBtn.className = 'btn btn-primary btn-sm float-right delete';
 
-  // Append text node
+    // Append text node
      editBtn.appendChild(document.createTextNode('Edit'));
 
-  // Append button to li
+    // Append button to li
      li.appendChild(editBtn);
-  // Append li to list
-   //  itemList.appendChild(li);
-  
+      
   }
 }
 // Remove item
 function removeItem(e){
-  if(e.target.classList.contains('delete')){
     
-      var li = e.target.parentElement;      
-      localStorage.removeItem(myObj.email);
-      userList.removeChild(li);       
-      //localStorage.clear()
-  }
+    var email = ''
+    if(e.target.classList.contains('delete')){
+    var li = e.target.parentElement;      
+    var str = (li.textContent);
+    var name = '';
+    var j;
+    for(var i = 0;i<str.length;i++)
+    {
+        if(str[i] == '-')
+      {
+        name = str.substring(0,i)
+        j = i + 1;
+        while(j<str.length && str[j] != ' ')
+        {      
+        email = email + str[j]; 
+        j++; 
+        } 
+      }         
+    }
+    userList.removeChild(li);
+    axios.get('https://crudcrud.com/api/9f269f43c52442c8887cd5c6912fd766/BooKAppointment')
+    .then((res) =>{
+      console.log(res.data[0]);
+        for(var i = 0;i<res.data.length;i++)
+        {
+            //console.log(res.data[0])
+            if(res.data[i].email === email)
+            {
+            console.log("Gone inside")
+            const val = res.data[i]._id;
+            axios.delete(`https://crudcrud.com/api/9f269f43c52442c8887cd5c6912fd766/BooKAppointment/${val}`)
+            .then((res)=>{
+            
+            })
+            .catch((error)=>
+            {
+                console.log(error)
+            })
+            break;
+        }
+        }
+    })
+    //axios.delete()
+    }
 }
-  // Edit item
+
+// Edit item
 function editItem(e){ 
-  if(e.target.classList.contains('edit')){
-    
-      
-      var li = e.target.parentElement;      
-      localStorage.removeItem(myObj.email);
-      userList.removeChild(li);     
-      onSubmit(e);  
-      //localStorage.clear()
-  }
+     
+    if(e.target.classList.contains('edit')){    
+    var li = e.target.parentElement;      
+    //localStorage.removeItem(myObj.email);
+    //userList.removeChild(li);     
+    //onSubmit(e);  
+    }
 }
