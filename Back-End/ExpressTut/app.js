5e5802c5edcadaa5461b1e9366ea41aa8e70a140
Shashwat1199 <shashwatsanskar@gmail.com>
2023-09-26 18:25:39 +0530

    Form3

--- Back-End/ExpressTut/app.js
+++ Back-End/ExpressTut/app.js
@@ -1,23 +1,20 @@
 const express = require('express');
 const app = express();
+
 const port = 80;
 const bodyParser = require('body-parser');
 
+const adminRouter= require('./routes/admin')
+const shopRouter= require('./routes/shop')
+
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/add-product',(req, res, next) =>{
-    
-    res.send('<form action = "/product" method = "POST"><input type = "text" name = "title"><br><input type = "number" name = "quantity"><button type ="submit">Add Product</button></form>')
- });
+app.use(adminRouter); 
+app.use(shopRouter);
 
- app.use('/product',(req,res,next)=>{
-    console.log(req.body)
-    res.redirect('/');
- })
-
- app.use('/',(req,res,next)=>{
-    res.send('<h3>Hello From NodeJS</h3>');
- });
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page Not Found</h1>')
+})
 
 app.listen(port,()=>{
 
---
+++ Back-End/ExpressTut/routes/shop.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router() 
+
+
+
+router.get('/',(req,res,next)=>{
+    res.send('<h3>Hello From NodeJS</h3>');
+ });
+
+ module.exports = router;
