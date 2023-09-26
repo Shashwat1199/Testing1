849d4a66ebf34d4512c36061ca653d04100681b3
Shashwat1199 <shashwatsanskar@gmail.com>
2023-09-26 17:41:06 +0530

    Form2

--- Back-End/ExpressTut/app.js
+++ Back-End/ExpressTut/app.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const app = express();
 const port = 80;
+const bodyParser = require('body-parser');
 
-app.use((req, res, next) =>{
-   console.log("This is first middleware");
-   next();
-});
+app.use(bodyParser.urlencoded({extended:false}));
 
-app.use((req, res, next) =>{
-    console.log("This is another middleware");
+app.use('/add-product',(req, res, next) =>{
+    
+    res.send('<form action = "/product" method = "POST"><input type = "text" name = "title"><br><input type = "number" name = "quantity"><button type ="submit">Add Product</button></form>')
+ });
+
+ app.use('/product',(req,res,next)=>{
+    console.log(req.body)
+    res.redirect('/');
+ })
+
+ app.use('/',(req,res,next)=>{
+    res.send('<h3>Hello From NodeJS</h3>');
  });
 
 app.listen(port,()=>{
